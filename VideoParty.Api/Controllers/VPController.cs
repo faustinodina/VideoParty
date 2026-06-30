f3020d2980b2d954c4f86c5522e3bb3738ce0c99
fdina <faustino.dina@gmail.com>
2026-06-30 00:36:58 -0400

    CreateParty aoi endpoint

--- VideoParty.Api/Controllers/VPController.cs
+++ VideoParty.Api/Controllers/VPController.cs
@@ -1,11 +1,35 @@
-﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using VideoParty.DataAccess.Data;
+using VideoParty.Model.Models;
 
 namespace VideoParty.Api.Controllers
 {
-  [Route("api/[controller]")]
+  [Route("[controller]")]
   [ApiController]
   public class VPController : ControllerBase
   {
+    private readonly ApplicationDbContext _db;
+
+    public VPController(ApplicationDbContext db)
+    {
+      _db = db;
+    }
+
+    [HttpPost("parties")]
+    public async Task<ActionResult<Party>> CreateParty(CreatePartyRequest request)
+    {
+      var party = new Party
+      {
+        PartyId = Guid.NewGuid(),
+        Name = request.Name
+      };
+
+      _db.Parties.Add(party);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(CreateParty), new { id = party.PartyId }, party);
+    }
   }
+
+  public record CreatePartyRequest(string Name);
 }
